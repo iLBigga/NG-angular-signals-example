66e183e3ba0b9d044299558df3131c254dbc17ee
Lorenzo Giannanti <giannanti1996@gmail.com>
2024-09-23 23:51:22 +0200

    NG-singals-example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,71 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { signal, effect, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'angular-signals-example';
+
+  theme = signal('light');
+
+  label = this.theme();
+
+  price = 19;
+
+  quantity = signal(10);
+
+  totalPrice = computed(() => this.price * this.quantity());
+
+  products = signal([
+    {id: 1, name: "Milk", price: 1.45},
+    {id: 2, name: "Bread", price: 3.90},
+    {id: 3, name: "Tomatoes", price: 2.20},
+  ]);
+
+  filterName = signal('');
+
+  filteredProducts = computed(() => {
+    return this.products().filter(
+      product => product.name
+      .toLowerCase()
+      .includes(this.filterName().toLowerCase()))
+  });
+
+  changeFilter(event: Event){
+    let newFilterName = (event.target as HTMLInputElement).value;
+    this.filterName.set(newFilterName);
+  }
+
+  changeQuantity(event : Event){
+    this.quantity.set((event.target as HTMLInputElement).valueAsNumber);
+  }
+
+  constructor(){
+    effect(() => {
+
+      this.label = this.theme();
+
+    });
+  }
+  ngOnInit(){
+
+    // this.theme.set(this.theme() === 'light'? 'dark' : 'light');
+
+    // this.theme.update(currentValue => currentValue === 'light'? 'dark' : 'light');
+
+    // document.body.className = this.theme();
+  }
+
+  toggleDarkMode() {
+    this.theme.update(currentValue => currentValue === 'light'? 'dark' : 'light');
+
+  }
 }
